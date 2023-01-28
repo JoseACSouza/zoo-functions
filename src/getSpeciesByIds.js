d42bf130e77d79cb3fc51529141c5d84c1b7743d
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-01-28 19:52:17 -0300

    realizando o primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  const specieId = data.species;
+  const idsList = ids;
+  return idsList.map((animals, index) => specieId.filter((id) => (id.id === ids[index]))[0]);
 };
-
 module.exports = getSpeciesByIds;
