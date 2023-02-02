f0fc9b30a380f802a87e45310fc5c522a781a291
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-02-02 15:25:30 -0300

    completa o requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    const newObject = {};
+    data.species.forEach((specie) => {
+      const { name, residents } = specie;
+      newObject[name] = residents.length;
+    });
+    return newObject;
+  }
+  const { species, sex } = animal;
+  if (sex) {
+    const getAnimal = data.species.find((specie) => specie.name === species);
+    return getAnimal.residents.filter((resident) => resident.sex === sex).length;
+  }
+  const getAnimal = data.species.find((specie) => specie.name === species);
+  return getAnimal.residents.length;
 };
-
 module.exports = countAnimals;
