6c7be96321f1526ebdc46aa602e4048d7dd5e55c
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-02-02 20:04:13 -0300

    completa o requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
+const countEntrants = (entrants = 0) => {
+  if (JSON.stringify(entrants) === '{}') {
+    return 0;
+  }
+  const result = {};
+  const yo = entrants.map((client) => {
+    const { age } = client;
+    return age;
+  });
+  const numberOfChilds = yo.filter((nChild) => nChild < 18).length;
+  const numberOfAdults = yo.filter((nAdult) => nAdult >= 18 && nAdult < 50).length;
+  const numberOfSeniors = yo.filter((nSenior) => nSenior >= 50).length;
+  if (numberOfChilds) {
+    result.child = numberOfChilds;
+  } if (numberOfAdults) {
+    result.adult = numberOfAdults;
+  } if (numberOfSeniors) {
+    result.senior = numberOfSeniors;
+  } return result;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  }
+  const { child = 0, adult = 0, senior = 0 } = countEntrants(entrants);
+  return child * data.prices.child + adult * data.prices.adult + senior * data.prices.senior;
 };
-
 module.exports = { calculateEntry, countEntrants };
